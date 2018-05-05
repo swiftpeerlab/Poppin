8207d5e869251fc0d0c2cfd9c28d496a00fcc20c
Tiago Martinho <martinho@thoughtworks.com>
2018-05-05 11:31:01 +0200

    Add MKMapView as ViewController view

--- Poppin/ViewController.swift
+++ Poppin/ViewController.swift
@@ -1,4 +1,10 @@
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view = MKMapView()
+    }
 }
--- PoppinTests/PoppinTests.swift
+++ PoppinTests/PoppinTests.swift
@@ -1,36 +1,13 @@
-//
-//  PoppinTests.swift
-//  PoppinTests
-//
-//  Created by Tiago Martinho on 4/5/18.
-//  Copyright © 2018 spl. All rights reserved.
-//
-
 import XCTest
+import MapKit
 @testable import Poppin
 
 class PoppinTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testMainViewShouldBeMapKitView() {
+        let viewController = ViewController()
+
+        let mapView = viewController.view
+
+        XCTAssert(mapView is MKMapView)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
